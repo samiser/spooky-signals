65537323befe7c4d7eda359f4003f8f7e8d97f7e
wookbee <159189510+wookbee@users.noreply.github.com>
2026-04-12 13:56:53 +0100

    removed print command from trigger.gd

--- trenchbroom/scripts/trigger.gd
+++ trenchbroom/scripts/trigger.gd
@@ -1,16 +1,18 @@
 extends Reciever
 signal send_signal
-@export var func_godot_properties : Dictionary
-var signal_ID : String
-var signal_parameter : String
-var triggered := false
-var is_allowed := true
-@onready var area: Area3D = $"."
+
+@export var func_godot_properties	: Dictionary
+@onready var area					: Area3D = $"."
+
+var signal_ID						: String
+var signal_parameter				: String
+var triggered						: bool = false
+var is_allowed						: bool = true
 
 func _ready() -> void:
-	signal_ID = func_godot_properties.get("signal_ID", "0");
-	signal_parameter = func_godot_properties.get("signal_parameter", "0");
-	is_allowed = func_godot_properties.get("autoallow", true);
+	signal_ID			= func_godot_properties.get("signal_ID", "0");
+	signal_parameter	= func_godot_properties.get("signal_parameter", "0");
+	is_allowed			= func_godot_properties.get("autoallow", true);
 	
 	area.body_entered.connect(_on_area_3d_body_entered)
 	connect_senders(func_godot_properties.get("recieve_signal_ID", "0"), signal_recieved)
@@ -20,7 +22,6 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		return
 	
 	if !triggered or  func_godot_properties.get("repeatable", true):
-		print(name + " entered, sigID: " + signal_ID + ", param: " + signal_parameter)
 		send_signal.emit(signal_parameter)
 		triggered = true
 
